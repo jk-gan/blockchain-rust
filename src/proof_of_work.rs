9ae8ac66358ffabd7bfc2a42fab1b966b4a61131
Gan Jun Kai <kuhn96@gmail.com>
2018-12-02 02:00:09 +0800

    add pow structure

--- src/proof_of_work.rs
+++ src/proof_of_work.rs
@@ -1,5 +1,17 @@
 use crate::block::Block;
+use num::bigint::BigUint;
+use num::traits::identities::One;
+
+const TARGER_BIT: usize = 24;
 
 struct ProofOfWork {
     block: Block,
+    target: BigUint,
+}
+
+impl ProofOfWork {
+    fn new(block: Block) -> Self {
+        let target = BigUint::one() << (256 - TARGER_BIT);
+        ProofOfWork { block, target }
+    }
 }
